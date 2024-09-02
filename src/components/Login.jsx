a125f688a3b4f9577618b1cdc6d7785a496b7e67
Piyush Rai <111386563+piyushrai007@users.noreply.github.com>
2024-09-02 20:20:30 +0000

    api

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import api from '../api/api';
+import api from './api/api';
 import './user_components/StudentDashboard.css';
 import Modal from 'react-modal';
 import Loading from './Loading';
